f3e781cb2fa1364ba46908ef04346da5b9ebf04b
Max Linke <max_linke@gmx.de>
2017-11-24 22:03:32 +0100

    add changelog and license and setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from Cython.Build import cythonize
+import numpy as np
+
+try:
+    numpy_include = np.get_include()
+except AttributeError:
+    numpy_include = np.get_numpy_include()
+
+setup(
+    name='pydiffusion',
+    author='Max Linke',
+    version='1.0dev',
+    license='GPLv3',
+    install_requires=['numpy', 'numba', 'MDAnalysis',],
+    packages=find_packages(),
+    include_dirs=[numpy_include],
+    ext_modules=cythonize('pydiffusion/quaternionsimulation.pyx')
+)
