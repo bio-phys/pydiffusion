f356be8a580ccfd59a263bae5900a41387ad4d60
Max Linke <max_linke@gmx.de>
2017-11-24 22:21:00 +0100

    add translations module

---
+++ pydiffusion/tests/test_translation.py
@@ -0,0 +1,124 @@
+# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 fileencoding=utf-8
+#
+# pydiffusion
+# Copyright (c) 2017 Max Linke and contributors
+# (see the file AUTHORS for the full list of names)
+#
+# pydiffusion is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pydiffusion is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pydiffusion.  If not, see <http://www.gnu.org/licenses/>.
+import numpy as np
+from os.path import join as pjoin, dirname
+import MDAnalysis as mda
+from collections import namedtuple
+
+import pytest
+from numpy.testing import assert_array_almost_equal
+
+from hummer.random import random_walk
+import hummer.diffusion.translation as hdt
+
+DATA_DIR = pjoin(dirname(__file__), 'data')
+
+
+class TestMSD:
+    def test_msd_arange_slow(self):
+        a = np.arange(9)
+        msd = hdt.msd(a, fft=False)
+        assert 8 == len(msd)
+        assert_array_almost_equal((a**2)[:-1], msd)
+
+    def test_msd_arange_fft(self):
+        a = np.arange(90)
+        msd = hdt.msd(a)
+        assert (len(a) - 1) == len(msd)
+        # for a few values the result is off by about 0.33. I don't know where
+        # this comes from but this solution is only a good aproxximation for
+        # large values of N anyway
+        assert_array_almost_equal((a**2)[:-1], msd, decimal=0)
+
+    def test_msd_constant_slow(self):
+        a = np.ones(9) * 42
+        msd = hdt.msd(a, fft=False)
+        assert_array_almost_equal(np.zeros(len(a) - 1), msd)
+
+    def test_msd_constant_fft(self):
+        a = np.ones(9) * 42
+        msd = hdt.msd(a)
+        assert_array_almost_equal(np.zeros(len(a) - 1), msd)
+
+    def test_same_result(self):
+        a = random_walk(1, 10000, 508842746).reshape(10000)
+        nlags = 100
+        msd_slow = hdt.msd(a, nlags=nlags, fft=False)
+        msd_fast = hdt.msd(a, nlags=nlags, fft=True)
+        assert_array_almost_equal(msd_slow, msd_fast)
+
+        b = random_walk(3, 10000)
+        msd_slow = hdt.msd(b, nlags=nlags, fft=False)
+        msd_fast = hdt.msd(b, nlags=nlags, fft=True)
+        assert_array_almost_equal(msd_slow, msd_fast)
+
+    def test_array_like(self):
+        a = list(np.arange(9))
+        msd = hdt.msd(a, fft=False)
+        assert 8 == len(msd)
+        assert_array_almost_equal(np.power(a, 2)[:-1], msd)
+
+    def test_ValueError(self):
+        a = np.ones((3, 3, 3))
+        with pytest.raises(ValueError):
+            hdt.msd(a)
+
+
+@pytest.fixture
+def curve():
+    # Code to generate trajectory from any atomgroup `s`
+    # def rotate(ag, R):
+    #     cg = ag.center_of_geometry()
+    #     ag.translate(-cg)
+    #     ag.rotate(R)
+    #     ag.translate(cg)
+    #     return ag
+    #
+    # s.translate(-s.center_of_geometry())
+    # pa = s.principal_axes()
+    # s.rotate(pa)
+    # s.write('curve.pdb')
+    # n_frames = 27
+    # r = 20
+    # trans = np.array([2 * np.pi * r / n_frames, 0, 10])
+    # rot1=mda.lib.transformations.rotation_matrix(
+    #     2 * np.pi / n_frames,(0, 0, 1))[:3, :3]
+    # rot[np.abs(rot) < 1e-12] = 0
+    # with mda.Writer('curve.xtc', s.n_atoms) as w:
+    #     s.translate([r, 0, 0])
+    #     w.write(s)
+    #     for i in range(n_frames):
+    #         s = rotate(s, rot1.T)
+    #         s.translate(trans)
+    #         trans = np.dot(trans, rot1.T)
+    #         w.write(s)
+    u = mda.Universe(pjoin(DATA_DIR, 'curve.pdb'),
+                     pjoin(DATA_DIR, 'curve.xtc'))
+    n_frames = u.trajectory.n_frames - 1  # stored an extra frame
+    trans = np.array([2 * np.pi * 20 / n_frames, 0, 0])
+    return namedtuple('TT', 'atoms, trans')(u.atoms, trans)
+
+
+@pytest.mark.parametrize('rot', [None, np.eye(3)])
+def test_ParticleCS(curve, rot):
+    pcs = hdt.ParticleCS(curve.atoms, rotation=rot).run()
+    diff = np.diff(pcs.pcs, axis=0)
+    assert_array_almost_equal(diff, np.ones(diff.shape) * diff[0], decimal=2)
+    assert_array_almost_equal(diff[0], curve.trans, decimal=2)
---
+++ pydiffusion/translation.py
@@ -0,0 +1,211 @@
+# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 fileencoding=utf-8
+#
+# pydiffusion
+# Copyright (c) 2017 Max Linke and contributors
+# (see the file AUTHORS for the full list of names)
+#
+# pydiffusion is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pydiffusion is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pydiffusion.  If not, see <http://www.gnu.org/licenses/>.
+from __future__ import division, absolute_import
+import numpy as np
+from MDAnalysis.analysis import align
+from MDAnalysis.analysis.base import AnalysisBase
+
+from .util.timeseries import _acf
+from .util.mda import parse_common_selection
+from .rotation import pcs
+from .util import hydropro
+
+__all__ = ['msd']
+
+
+def _msd(x, nlags):
+    """
+    calculate msd with the normal sum approach
+    """
+    MSD = np.zeros(nlags)
+    for i in range(1, nlags):
+        MSD[i] = np.mean(np.sum((x[i:] - x[:-i])**2, 1))
+    return MSD
+
+
+def _msd_fft(x, nlags):
+    """
+    calculate msd with an fft
+
+    References
+    ----------
+    Calandrini, V., Pellegrini, E., Calligari, P., Hinsen, K., & Kneller, G. R.
+    (2011). nMoldyn-Interfacing spectroscopic experiments, molecular dynamics
+    simulations and models for time correlation functions. Collection SFN, 12,
+    201–232. http://doi.org/10.1051/sfn/201112010
+    """
+    N, ndim = x.shape
+    x2 = x**2
+
+    zeros = np.zeros(ndim)
+    x2 = np.vstack((zeros, x2, zeros))
+    x2 = x2.sum(axis=1)
+
+    s_ab = np.sum(
+        [_acf(_x, N, demean=False, norm=False) for _x in x.T], axis=0)
+
+    MSD = 2 * x2.sum() + np.cumsum(-x2[:N] - x2[:1:-1])
+    return (MSD / (N - np.arange(N)) - 2 * s_ab)[:nlags]
+
+
+def msd(x, nlags=None, fft=True):
+    r"""Calculate the MSD for a 1D trajectory.
+
+    .. math:
+    MSD(m) = \frac{1}{N - m} \sum_{k=0}^{N-m-1} [r(k+m) - r(k)]^2
+
+    By default a fast FFT algorithm is used to calculate the MSD. This is an
+    approximation that is normally the same as the standard algorithm upto
+    numerical round-off errors. It might deviate for short trajectories. This
+    algorithm will always calculate the MSD for all values. If you are only
+    interested in the first few values the standard algorithm might be faster.
+
+    Parameters
+    ----------
+    x : array-like
+        trajectory. Should either be 1D or 2D with (nframes, ndim)
+    nlags : int (optional)
+        return MSD for nlags frames
+    fft : bool (optional)
+        use FFT to calclate MSD
+
+    Returns
+    -------
+    msd : np.ndarray
+        mean square displacement
+
+    References
+    ----------
+    Calandrini, V., Pellegrini, E., Calligari, P., Hinsen, K., & Kneller, G. R.
+    (2011). nMoldyn-Interfacing spectroscopic experiments, molecular dynamics
+    simulations and models for time correlation functions. Collection SFN, 12,
+    201–232. http://doi.org/10.1051/sfn/201112010
+
+    """
+    x = np.asarray(x)
+    if nlags is None:
+        nlags = len(x) - 1
+
+    if x.ndim == 1:
+        x = x.reshape(len(x), 1)
+    elif x.ndim > 2:
+        raise ValueError('MSD can only be calculated for 1D or 2D arrays')
+
+    if fft:
+        return _msd_fft(x, nlags)
+    else:
+        return _msd(x, nlags)
+
+
+class TranslationTensor(object):
+    def __init__(self, D, R):
+        D = np.array(D)
+        if D.shape != (3, ):
+            raise ValueError("D shape should be (3,)")
+        self._D = D
+
+        if R is not None:
+            R = np.asarray(R)
+            if R.shape != (3, 3):
+                raise ValueError("R shape should be (3, 3)")
+        self._R = R
+
+    @property
+    def D(self):
+        return self._D
+
+    @property
+    def R(self):
+        return self._R
+
+    @classmethod
+    def from_hydropro(cls, res_file):
+        transD = hydropro.read_diffusion_tensor(res_file)[:3, :3]
+        D, R = pcs(transD)[:2]
+        return cls(D, R)
+
+    @property
+    def tensor(self):
+        return np.dot(self.R, np.dot(np.eye(3) * self.D, self.R.T))
+
+
+class ParticleCS(AnalysisBase):
+    """Transform a trajectory of a rigid body in the laboratory coordinate system
+    into the particle coordinate system (PCS). This removes all rotations and
+    recovers the pure translational movement in the PCS.
+
+    Attributes
+    ----------
+    pcs : ndarray
+        trajectory in the PCS
+
+    """
+
+    def __init__(self, mobile, reference=None, rotation=None, **kwargs):
+        """Parameter
+        ---------
+        mobile : mda.Atomgroup
+            Atomgroup defining a rigid-body
+        reference : mda.Atomgroup, optional
+            Reference Atomgroup. If none is given use mobile at time 0
+        rotation : array-like, optional
+            rotation matrix to transform the particle at time 0 from the LCS to
+            the PCS. If none is given the identiy matrix is used (this assume
+            the particle is in orientated in the PCS at the beginning)
+        start/stop/step : int
+            trajectory indexing
+        """
+        # The run() method should start one frame after start since I need the
+        # first frame for setting up the analysis. So do some manual conversion
+        # of the start and step
+        if 'start' not in kwargs:
+            kwargs['start'] = 1
+        # TODO: check correct beginning frame with the step
+        if 'step' in kwargs:
+            kwargs['start'] += kwargs['step']
+        super(ParticleCS, self).__init__(mobile.universe.trajectory, **kwargs)
+        # I'm analysing one frame more then `_setup_frames` thinks
+        self.n_frames += 1
+
+        self._mobile, self._ref = parse_common_selection(
+            mobile.universe, mobile, reference)
+        if rotation is None:
+            self._rotation = np.eye(3)
+        else:
+            self._rotation = np.asarray(rotation)
+
+    def _prepare(self):
+        self._dx = []
+        self._trajectory[self.start - self.step]
+        self._pos_prev = self._mobile.positions.copy()
+
+    def _single_frame(self):
+        pos = self._mobile.positions.copy()
+        com = pos.mean(0)
+        com_prev = self._pos_prev.mean(0)
+        diff = com - com_prev
+
+        R = align.rotation_matrix(self._pos_prev - com_prev,
+                                  self._ref.positions)[0]
+        self._dx.append(np.dot(diff, np.asarray(R).T))
+        self._pos_prev = pos
+
+    def _conclude(self):
+        self.pcs = np.dot(np.asarray(self._dx), self._rotation.T).cumsum(0)
