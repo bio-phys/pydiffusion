4590bef1f404224a1ad55a119f34a5b40ce8b8a0
Max Linke <max_linke@gmx.de>
2017-11-24 22:30:28 +0100

    add quaternion brownian dynamics simulation

---
+++ pydiffusion/quaternionsimulation.pyx
@@ -0,0 +1,281 @@
+# -*- Mode: cython; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 fileencoding=utf-8
+#
+# pydiffusion
+# Copyright (c) 2017 Max Linke and contributors
+# (see the file AUTHORS for the full list of names)
+#
+# pydiffusion is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pydiffusion is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pydiffusion.  If not, see <http://www.gnu.org/licenses/>.
+import numpy as np
+cimport numpy as np
+cimport cython
+from scipy._lib._util import check_random_state
+
+from libc.math cimport sqrt
+
+ctypedef np.double_t DTYPE_t
+DTYPE = np.double
+
+
+##################################################
+# Quaternion implementation with python wrappers #
+##################################################
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef DTYPE_t c_norm(DTYPE_t[::1] q):
+    cdef DTYPE_t s = 0
+    cdef int i
+    for i in range(4):
+        s += q[i] * q[i]
+    return sqrt(s)
+
+
+def norm(q):
+    """norm of a quaternion (s, v1, v2, v3)
+
+    Parameters
+    ----------
+    q : array-like
+        4 element array, initial quaternion
+
+    Returns
+    -------
+    norm : float
+    """
+    return c_norm(np.asarray(q, dtype=DTYPE, order='C'))
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef void c_conjugate(DTYPE_t[::1] q, DTYPE_t[::1] res):
+    res[0] = q[0]
+    res[1] = -q[1]
+    res[2] = -q[2]
+    res[3] = -q[3]
+
+
+def conjugate(q):
+    """conjugate of a quaternion (s, v1, v2, v3)
+
+    conjugate(q) = (s, -v1, -v2, -v3)
+
+    Parameters
+    ----------
+    q : array-like
+        4 element array, initial quaternion
+
+    Returns
+    -------
+    np.ndarray (4)
+        conjugate quaternion
+    """
+    res = np.empty(4, dtype=DTYPE)
+    c_conjugate(np.asarray(q, dtype=DTYPE, order='C'), res)
+    return res
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef void c_inv(DTYPE_t[::1] q, DTYPE_t[::1] res):
+    cdef DTYPE_t norm2 = 0
+    cdef int i
+    for i in range(4):
+        norm2 += q[i] * q[i]
+    c_conjugate(q, res)
+    for i in range(4):
+        res[i] /= norm2
+
+
+def inv(q):
+    """ inverse quaternions
+
+    Parameters
+    ----------
+    q : array-like
+        4 element array, initial quaternion
+
+    Returns
+    -------
+    np.ndarray (4)
+        inverse quaternion
+
+    """
+    res = np.empty(4, dtype=DTYPE)
+    q = np.asarray(q, dtype=DTYPE)
+    c_inv(q, res)
+    return res
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef void c_mul(DTYPE_t[::1] q0, DTYPE_t[::1] q1, DTYPE_t[::1] qq):
+    qq[0] = q1[0]*q0[0] - q1[1]*q0[1] - q1[2]*q0[2] - q1[3]*q0[3]
+    qq[1] = q1[0]*q0[1] + q1[1]*q0[0] + q1[2]*q0[3] - q1[3]*q0[2]
+    qq[2] = q1[0]*q0[2] - q1[1]*q0[3] + q1[2]*q0[0] + q1[3]*q0[1]
+    qq[3] = q1[0]*q0[3] + q1[1]*q0[2] - q1[2]*q0[1] + q1[3]*q0[0]
+
+
+def mul(q1, q2):
+    """ multiply two quaternions
+
+    Parameters
+    ----------
+    q1 : array-like
+        4 element array, quaternion
+    q2 : array-like
+        4 element array, quaternion
+
+    Returns
+    -------
+    q : np.ndarray (4)
+        result of multiplication
+    """
+    res = np.empty(4, dtype=DTYPE)
+    c_mul(np.asarray(q1, dtype=DTYPE, order='C'),
+          np.asarray(q2, dtype=DTYPE, order='C'),
+          res)
+    return res
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cdef void c_rotate_by(DTYPE_t[::1] q, DTYPE_t[::1] vq, DTYPE_t[::1] res,
+                      DTYPE_t[::1] tmp0, DTYPE_t[::1] tmp1):
+    """
+    vq = vector transformed to quaternion
+
+    allocate temporaties outside of this function
+    """
+    # tmp = q * vq
+    c_mul(q, vq, tmp0)
+    # tmp1 = conjugate(q)
+    c_conjugate(q, tmp1)
+    # res = (q * vq) * conjugate(q)
+    c_mul(tmp0, tmp1, res)
+
+
+def rotate_by(q, vec):
+    """rotation vector ``vec`` using quaternion ``q``
+
+    Parameters
+    ----------
+    q : array-like
+        quaternion
+    vec : array-like
+        vector
+
+    Returns
+    -------
+    vec : array-like
+        rotated vector
+    """
+    q = np.asarray(q, dtype=DTYPE)
+    vec = np.asarray(vec, dtype=DTYPE)
+
+    qvec = np.zeros(4, dtype=DTYPE)
+    res = np.zeros(4, dtype=DTYPE)
+    tmp0 = np.zeros(4, dtype=DTYPE)
+    tmp1 = np.zeros(4, dtype=DTYPE)
+
+    qvec[1:] = vec
+
+    c_rotate_by(q, qvec, res, tmp0, tmp1)
+
+    return res[1:]
+
+
+def quat(axis, angle):
+    """ quaternion from axis and angle
+
+    Parameters
+    ----------
+    axis : array-like
+        vector along to rotate
+    angle : float
+        angle in degree
+
+    Returns
+    -------
+    q : ndarray (4)
+        quaternion to describe rotation
+    """
+    axis /= np.linalg.norm(axis)
+    quat = np.empty(4, dtype=DTYPE)
+    angle = np.radians(.5 * angle)
+    quat[0] = np.cos(angle)
+    quat[1:] = np.sin(angle) * axis
+    return quat
+
+def quaternion_matrix(q):
+    cdef DTYPE_t[::1] u = np.asarray(q, dtype=DTYPE)
+    cdef DTYPE_t[:, ::1] m = np.empty((3, 3))
+
+    m[0, 0] = 1 - 2 * (u[2]*u[2] + u[3]*u[3])
+    m[0, 1] = 2 * (u[0]*u[3] + u[1]*u[2])
+
+    return m
+
+###########################
+# Rotation diffusion code #
+###########################
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def run(D, niter, dt, random_state=None):
+    """Brownian dynamics simulation of the rotation of a point on the sphere.
+    Results are recorded as rotations relative to any starting point.
+
+    Parameters
+    ----------
+    D : array-like (3)
+        diagonalized elements of diffusion tensor
+    niter : int
+        number of iterations
+    dt : float
+        timestep in seconds
+    random_state : int (optional)
+        seed for rng. If ``None`` a random seed is chosen. Note for
+        multiproccesing using explicit seeds is recommended.
+
+    Returns:
+    q : ndarray (niter, 4)
+        rotation trajectory in quaternions
+    """
+    D = np.asarray(D, dtype=DTYPE)
+
+    if dt <= 0:
+        raise ValueError("dt must be larger then 0")
+
+    if niter <= 0:
+        raise ValueError("niter must be larger then 0")
+
+    rng = check_random_state(random_state)
+    a = np.sqrt(.5 * D * dt)
+
+    q = np.empty((niter, 4), dtype=DTYPE)
+    q[:, 1:] = a * rng.normal(size=(niter, 3))
+    q[:, 0] = np.sqrt(1 - .5 * D.sum() * dt)
+    q /= np.sqrt((q**2).sum(1))[:, np.newaxis]
+    q[0] = np.array([1, 0, 0, 0], dtype=DTYPE)
+
+    cdef DTYPE_t[:, ::1] view_q = q
+    cdef DTYPE_t[::1] q_new = np.empty(4, dtype=DTYPE)
+
+    cdef int i
+    for i in range(1, niter):
+        c_mul(view_q[i], view_q[i-1], q_new)
+        view_q[i] = q_new
+
+    return q
---
+++ pydiffusion/tests/test_quaternionsimulation.py
@@ -0,0 +1,136 @@
+# -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 fileencoding=utf-8
+#
+# pydiffusion
+# Copyright (c) 2017 Max Linke and contributors
+# (see the file AUTHORS for the full list of names)
+#
+# pydiffusion is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pydiffusion is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pydiffusion.  If not, see <http://www.gnu.org/licenses/>.
+import numpy as np
+from MDAnalysis.lib import transformations
+
+from numpy.testing import assert_array_equal, assert_array_almost_equal
+import pytest
+
+from hummer.simulation import rotation
+
+##################################
+# Test Quaternion implementation #
+##################################
+
+
+@pytest.fixture
+def zero_quat():
+    return [1, 0, 0, 0]
+
+
+@pytest.fixture
+def right_angle():
+    return rotation.quat([1, -1, 1], 90)
+
+
+@pytest.fixture
+def right_angle_mat():
+    return transformations.rotation_matrix(np.radians(90), [1, -1, 1])[:3, :3]
+
+
+@pytest.fixture
+def vec():
+    return np.ones(3) / np.sqrt(3)
+
+
+def test_norm(zero_quat):
+    assert 1 == rotation.norm(zero_quat)
+
+
+def test_conjugate():
+    q = [2, 1, 1, 1]
+    assert_array_equal([2, -1, -1, -1], rotation.conjugate(q))
+
+
+@pytest.mark.parametrize('axis, angle', (([1, 1, 1], 90), ([0, 1, 1], 45)))
+def test_quat(axis, angle):
+    q = rotation.quat(axis, angle)
+    axis = np.array(axis)
+    assert 1 == rotation.norm(q)
+    assert np.cos(np.radians(.5 * angle)) == q[0]
+    assert_array_almost_equal(
+        np.sin(np.radians(.5 * angle)) * axis / np.linalg.norm(axis), q[1:])
+    assert_array_almost_equal(
+        transformations.quaternion_about_axis(np.radians(angle), axis), q)
+
+
+def test_mul(zero_quat, right_angle):
+    assert_array_almost_equal(right_angle, rotation.mul(zero_quat,
+                                                        right_angle))
+    assert_array_almost_equal(right_angle, rotation.mul(right_angle,
+                                                        zero_quat))
+    assert_array_almost_equal(
+        transformations.quaternion_multiply(right_angle, right_angle),
+        rotation.mul(right_angle, right_angle))
+
+
+def test_rotate_by(right_angle, zero_quat, vec, right_angle_mat):
+    assert_array_almost_equal(vec, rotation.rotate_by(zero_quat, vec))
+    assert_array_almost_equal(
+        rotation.rotate_by(right_angle, vec),
+        rotation.rotate_by(-right_angle, vec))
+    assert_array_almost_equal(
+        np.dot(right_angle_mat.T, vec), rotation.rotate_by(right_angle, vec))
+
+
+def test_inverse(right_angle):
+    assert_array_almost_equal([1, 0, 0, 0], rotation.mul(
+        right_angle, rotation.inv(right_angle)))
+
+###################
+# Test Simulation #
+###################
+
+
+@pytest.fixture
+def D():
+    return np.asarray([30042854.0, 20255914.0, 21021232.0])
+
+
+@pytest.mark.parametrize('dt', (-1, 0))
+def test_run_exception(dt):
+    with pytest.raises(ValueError):
+        rotation.run(D=np.ones(3), niter=10, dt=dt, random_state=42)
+    with pytest.raises(ValueError):
+        rotation.run(D=np.ones(3), niter=dt, dt=1, random_state=42)
+
+
+@pytest.mark.parametrize('niter', (42, 1238))
+def test_run_return_size(D, niter):
+    r1 = rotation.run(D=D, niter=niter, dt=1e-9, random_state=42)
+    assert r1.shape == (niter, 4)
+
+
+@pytest.mark.parametrize('seed', (42, 1238230))
+def test_run_reproducibility(D, seed):
+    r1 = rotation.run(D=D, niter=10, dt=1e-9, random_state=seed)
+    r2 = rotation.run(D=D, niter=10, dt=1e-9, random_state=seed)
+    assert_array_equal(r1, r2)
+
+    r1 = rotation.run(D=D, niter=10, dt=1e-9)
+    r2 = rotation.run(D=D, niter=10, dt=1e-9)
+    assert not np.allclose(r1, r2)
+
+
+def test_run(D):
+    r1 = rotation.run(D=D, niter=10, dt=1e-9)
+    assert_array_almost_equal(r1[0], [1, 0, 0, 0])
+    for r in r1[1:]:
+        assert not np.allclose(r1[0], r)
