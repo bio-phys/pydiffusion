e79be075150f521be693162241796bfb085aba7f
Max Linke <max_linke@gmx.de>
2017-11-24 22:31:41 +0100

    add init

---
+++ pydiffusion/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+from . import translation
+from . import rotation
+from . import quaternionsimulations
+from . import util
+
+__all__ = ['translation', 'rotation', 'quaternionsimulations', 'util']
