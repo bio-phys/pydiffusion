835ea53b4e263bf31552e6c205a8a0367511bafb
Max Linke <max_linke@gmx.de>
2017-11-27 11:11:41 +0100

    enable coverage in cython extension

--- pydiffusion/quaternionsimulation.pyx
+++ pydiffusion/quaternionsimulation.pyx
@@ -1,3 +1,6 @@
+# cython: linetrace=True
+# cython: embedsignature=True
+# distutils: define_macros=CYTHON_TRACE=1
 # -*- Mode: cython; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 fileencoding=utf-8
 #
